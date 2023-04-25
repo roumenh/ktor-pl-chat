7d9e0b7cd9b360a1404ac91781d5ab0b765b21af
Roman Hruška <hruska.r@gmail.com>
2023-04-25 19:46:28 +0200

    create Room Controller

---
+++ src/main/kotlin/com/romanhruska/room/RoomController.kt
@@ -0,0 +1,60 @@
+package com.romanhruska.room
+
+import com.romanhruska.data.MessageDataSource
+import com.romanhruska.data.model.Message
+import io.ktor.websocket.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.util.concurrent.ConcurrentHashMap
+
+class RoomController (
+    private val messageDataSource: MessageDataSource
+) {
+    private val members = ConcurrentHashMap<String, Member>() // key is username, value is Member
+
+    fun onJoin(
+        username: String,
+        sessionId: String,
+        socket: WebSocketSession
+    ) {
+        if(members.containsKey(username)) {
+            throw MemberAlreadyExistsException()
+        }
+        members[username] = Member(
+            username = username,
+            sessionId = sessionId,
+            socket = socket
+        )
+    }
+
+    suspend fun sendMessage(senderUsername: String, message: String) {
+        members.values.forEach { member ->
+            // TODO why is the lambda here?
+            val messageEntity = Message(
+                text = message,
+                username = senderUsername,
+                timestamp = System.currentTimeMillis()
+            )
+            messageDataSource.insertMessage(messageEntity) // should this really be inside the lambda?
+
+            // now we want to send the message to all members in the room...
+            val parsedMessage = Json.encodeToString(messageEntity)  // import kotlinx.serialization.encodeToString
+            member.socket.send(Frame.Text(parsedMessage))
+        }
+    }
+
+    suspend fun getAllMessages(): List<Message> {
+        return messageDataSource.getAllMessages()
+    }
+
+    suspend fun tryDisconnect(username: String) {
+        members[username]?.socket?.close()
+
+        // remove room member from our hashmap
+        if(members.containsKey(username)) {
+            members.remove(username)
+        }
+
+    }
+
+}
