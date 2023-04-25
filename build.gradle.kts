322fb535243d1831f0d86aea89056fe7cbe84d4d
Roman Hruška <hruska.r@gmail.com>
2023-04-25 19:22:37 +0200

    clean up boilerplate code from routing and Serialization + added dependencies for kmongo and koin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val kmongo_version: String by project
+val koin_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.21"
@@ -32,4 +34,13 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    // Kmongo
+    implementation("org.litote.kmongo:kmongo:$kmongo_version")
+    implementation("org.litote.kmongo:kmongo-coroutine:$kmongo_version")
+
+    // Koin core features
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.insert-koin:koin-logger-slf4j:$koin_version")
 }
--- src/main/kotlin/com/romanhruska/plugins/Routing.kt
+++ src/main/kotlin/com/romanhruska/plugins/Routing.kt
@@ -5,9 +5,5 @@ import io.ktor.server.response.*
 import io.ktor.server.application.*
 
 fun Application.configureRouting() {
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-    }
+
 }
