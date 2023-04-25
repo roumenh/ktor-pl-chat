2d8fd3873bf273ab7bc579eeb91fd3942686437d
Roman Hruška <hruska.r@gmail.com>
2023-04-25 19:25:26 +0200

    Create Message data class

---
+++ src/main/kotlin/com/romanhruska/data/model/Message.kt
@@ -0,0 +1,14 @@
+package com.romanhruska.data.model
+
+import kotlinx.serialization.Serializable
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+
+@Serializable
+data class Message(
+    val text: String,
+    val username: String,
+    val timestamp: Long,
+    @BsonId
+    val id: String = ObjectId().toString()  //will generate random object ID
+)
